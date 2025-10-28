7395f6b54a0ebcfcb9be57373ce9fbd8bac0a1c8
Jonas Perez <00jonasperez@gmail.com>
2025-10-28 22:27:28 +0800

    fix: country card layout issue

--- app/components/countries/CountryCard.tsx
+++ app/components/countries/CountryCard.tsx
@@ -69,24 +69,24 @@ export default function CountryCard({ country }: { country: any }) {
                 )}
             </figure>
             <div className="card-body">
-                <h2 className="card-title">{country.name.common}</h2>
-                <p className="mb-4">Vexillologist DSA mumblecore, same paleo etsy slow-carb meggings bicycle rights helvetica small batch. Etsy pinterest gochujang cred mustache. Kombucha raw denim marxism cardigan, dreamcatcher squid.</p>
+                <h2 className="card-title">{country.name.common}</h2>                
+                <div>
+                    <p className="mb-4">Vexillologist DSA mumblecore, same paleo etsy slow-carb meggings bicycle rights helvetica small batch. Etsy pinterest gochujang cred mustache. Kombucha raw denim marxism cardigan, dreamcatcher squid.</p>
+                    <div className="grid grid-cols-2">
+                        <ul>
+                            <li><span className="font-medium">Capital:</span> {country.capital.join(', ')}</li>
+                            <li><span className="font-medium">Continent:</span> {country.continents.join(', ')}</li>
+                            <li><span className="font-medium">Population:</span> {country.population.toLocaleString()}</li>
+                            <li><span className="font-medium">Timezones:</span> {country.timezones.join(', ')}</li>
+                        </ul>
 
-                <div className="flex justify-between">
-                    <ul>
-                        <li><span className="font-medium">Capital:</span> {country.capital.join(', ')}</li>
-                        <li><span className="font-medium">Continent:</span> {country.continents.join(', ')}</li>
-                        <li><span className="font-medium">Population:</span> {country.population.toLocaleString()}</li>
-                        <li><span className="font-medium">Timezones:</span> {country.timezones.join(', ')}</li>
-                    </ul>
-
-                    <ul>
-                        <li><span className="font-medium">Currencies:</span> {Object.keys(country.currencies)[0]}</li>
-                        <li><span className="font-medium">Continent:</span> {Object.values(country.continents).join(', ')}</li>
-                    </ul>
+                        <ul>
+                            <li><span className="font-medium">Currencies:</span> {Object.keys(country.currencies)[0]}</li>
+                            <li><span className="font-medium">Languages:</span> {Object.values(country.languages).join(', ')}</li>
+                        </ul>
+                    </div>
                 </div>
-
-                <div className="card-actions justify-end">
+                <div className="card-actions justify-end mt-auto">
                     <AddToBucketBtn country={country} />
                     <Link to={`/country/${country.cca2.toLowerCase()}`} className="btn btn-primary">More Info</Link>
                 </div>
