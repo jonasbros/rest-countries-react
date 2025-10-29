1ca77212ebd6cbfa0430e172d434fe67ebf2e77d
Jonas Perez <00jonasperez@gmail.com>
2025-10-29 19:21:00 +0800

    fix: adjust add to bucket list button label

--- app/components/countries/AddToBucketBtn.tsx
+++ app/components/countries/AddToBucketBtn.tsx
@@ -2,14 +2,14 @@ export default function AddToBucketBtn({ children, country }: { children?: strin
     return (
         <>
             <label className="btn btn-square btn-primary swap">
-                <input type="checkbox" />
+                <input id={`add-to-bucketlist__btn-${country.cca2}`} type="checkbox" />
                 {/* off */}
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2.5" stroke="currentColor" className="size-[1.2em] swap-off"><path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" /></svg>
                 {/* on */}
                 <svg xmlns="http://www.w3.org/2000/svg" fill="crimson" viewBox="0 0 24 24" strokeWidth="2.5" stroke="currentColor" className="size-[1.2em] swap-on"><path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" /></svg>
             </label>
             {children ? (
-                <span className="ml-2">{children}</span>                   
+                <label for={`add-to-bucketlist__btn-${country.cca2}`} className="ml-2 cursor-pointer transition-opacity duration-150 hover:opacity-80">{children}</label>                   
             ) : null}
         </>
     );
