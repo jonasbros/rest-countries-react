dfa45553b65fdc28a96aaa6dde742c3a897d5e7f
Jonas Perez <00jonasperez@gmail.com>
2025-10-30 02:11:25 +0800

    fix: add to bucket list button adjustments

--- app/components/countries/AddToBucketBtn.tsx
+++ app/components/countries/AddToBucketBtn.tsx
@@ -23,11 +23,12 @@ export default function AddToBucketBtn({ country, showLabel = false }: { country
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2.5" stroke="currentColor" className="size-[1.2em] swap-off"><path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" /></svg>
                 {/* on */}
                 <svg xmlns="http://www.w3.org/2000/svg" fill="crimson" viewBox="0 0 24 24" strokeWidth="2.5" stroke="currentColor" className="size-[1.2em] swap-on"><path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" /></svg>
+            
             </label>
             {showLabel ? (
                 <label
                     htmlFor={`add-to-bucketlist__btn-${country.cca2}`}
-                    className="ml-2 cursor-pointer transition-opacity duration-150 hover:opacity-80"
+                    className="ml-2 cursor-pointer select-none transition-opacity duration-150 hover:opacity-80"
                 >
                     { isInList ? 'Remove from' : 'Add to' } Bucket List
                 </label>                   
