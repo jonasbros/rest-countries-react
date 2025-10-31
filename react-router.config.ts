3a6bac6b1dcac6eaa06f7bad39cb2d1f8efd86c3
Jonas Perez <00jonasperez@gmail.com>
2025-10-31 21:28:04 +0800

    chore: remove custom build folder

--- react-router.config.ts
+++ react-router.config.ts
@@ -2,7 +2,6 @@ import type { Config } from "@react-router/dev/config";
 
 export default {
   // Config options...
-  buildDirectory: "docs",
   // Server-side render by default, to enable SPA mode set this to `false`
   ssr: true,
   future: {
